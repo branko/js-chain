429c6ec6155bf3cd27c04afdae0f0682463332cc
Branko Culum <brankoculum@gmail.com>
2018-05-19 14:45:29 -0400

    fixed typo

--- scripts/blockchain.js
+++ scripts/blockchain.js
@@ -30,7 +30,7 @@ class Blockchain {
   createGenesisBlock() {
     const genesisBlock = new Block('0', null, [
       new Transaction(null, RSA.getPublicKey(), 100),
-      new Transaction(null, fs.readFileSync('./keys/destination_key.pub'))
+      new Transaction(null, fs.readFileSync('./keys/destination_key.pub').toString())
     ]);
 
     genesisBlock.nonce = 0;
